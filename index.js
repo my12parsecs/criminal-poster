5e36b5eed138a142c00cc5acd1679d829eea278f
Hikaru <105549697+my12parsecs@users.noreply.github.com>
2023-09-13 22:25:35 +0900

    change

--- index.js
+++ index.js
@@ -1,19 +1,154 @@
-const nums = [1, 2, 3, 4, 5]
-const nuums = nums.map(function(hi){
-    return hi * hi})
-console.log(nuums);
+let photoDrag;
+let beforeDiv
 
-console.log("hello");
+const position = document.querySelectorAll(".position")
+const finish = document.querySelector(".finish")
+const scoreContainer = document.querySelector(".score-container")
 
-const names = ["alice", "bob", "charlie", "danielle"]
-const namees = names.map(function(item){
-    return item[0].toUpperCase() + item.slice(1)
+document.addEventListener("dragstart", (event) => {
+  photoDrag = event.target;
+  event.target.style.opacity = 0.6;
+  // console.log(event.target.parentNode);
+  beforeDiv = event.target.parentNode
+});
+
+// ドラッグ中
+document.addEventListener("drag", () => {});
+
+// ドロップ可能エリアに入った時
+document.addEventListener("dragenter", (event) => {
+  if (event.target.className == "position") {
+    event.target.style.background = "#a9a9a9";
+  }
+});
+
+document.addEventListener("dragover", (event) => {
+    event.preventDefault();
+  },
+  false
+);
+
+// ドロップ可能エリアから離れた時
+document.addEventListener("dragleave", (event) => {
+  if (event.target.className == "position") {
+    event.target.style.background = "";
+  }
+});
+
+// ドラッグが終了した時
+document.addEventListener("dragend", (event) => {
+  event.target.style.opacity = 1;
+});
+
+// ドロップ時の処理
+document.addEventListener("drop", (event) => {
+
+  // 上の候補地間での入れ替え用
+  if (event.target.className == "photo-each" || event.target.parentElement.className == "position") {
+    let targetPosition = event.target.parentElement
+    let beforeDrag = event.target
+    photoDrag.parentNode.removeChild(photoDrag);  
+    beforeDiv.appendChild(beforeDrag)
+    targetPosition.appendChild(photoDrag);
+  }
+  // 下と上を行き来する用
+  if (event.target.className == "position") {
+    event.target.style.background = "";
+    photoDrag.parentNode.removeChild(photoDrag);
+    if(event.target.innerHTML != ""){
+      let beforeDrag = event.target.innerHTML
+      beforeDiv.innerHTML = beforeDrag
+      event.target.innerHTML = ""
+    }
+    event.target.appendChild(photoDrag);
+  }
+  // 格納している変数を初期化
+  photoDrag = null;
+});
+
+
+
+
+finish.addEventListener("click", ()=>{
+  let score = 0
+  for(let i = 0; i < 4; i++){
+    if(position[i].innerHTML != ""){
+      if(position[i].id[5] == position[i].children[0].id){score++}
+    }
+  }
+  console.log(score);
+  scoreContainer.innerText = score
 })
-console.log(namees);
 
-const football = ["Hazard", "Neymar", "Thiago"]
-const baller = football.map(function(name){
-    return `<p>${name}</p>`
-})
-console.log(baller);
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// <お手本>
+// ドラッグしている要素を格納する変数
+let dragItem;
+
+// ドラッグが開始された時
+document.addEventListener("dragstart", (event) => {
+  // ドラッグした要素を変数に格納
+  dragItem = event.target;
+  event.target.style.opacity = 0.6;
+});
+
+// ドラッグ中
+document.addEventListener("drag", () => {});
+
+// ドロップ可能エリアに入った時
+document.addEventListener("dragenter", (event) => {
+  if (event.target.className == "box") {
+    event.target.style.background = "#a9a9a9";
+  }
+});
+
+// ドロップ可能エリア内にある時
+document.addEventListener("dragover", (event) => {
+    event.preventDefault();
+  },
+  false
+);
+
+// ドロップ可能エリアから離れた時
+document.addEventListener("dragleave", (event) => {
+  if (event.target.className == "box") {
+    event.target.style.background = "";
+  }
+});
+
+// ドラッグが終了した時
+document.addEventListener("dragend", (event) => {
+  event.target.style.opacity = 1;
+});
+
+// ドロップ時の処理
+document.addEventListener("drop", (event) => {
+  if (event.target.className == "box") {
+    event.target.style.background = "";
+    dragItem.parentNode.removeChild(dragItem);
+    event.target.appendChild(dragItem);
+  }
+  // 格納している変数を初期化
+  dragItem = null;
+});
